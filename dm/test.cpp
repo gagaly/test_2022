42761294e5d14579e5431250454acc7e7d35848e
gagaly <chesterteqo@gmail.com>
2022-06-30 16:18:23 +0800

    DM

---
+++ dm/test.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//测试指针打印指针地址
+//int main()
+//{
+//	
+//	int p=printf("hehelala\n");
+//	int* r = &p;
+//		printf("%d\n", p);
+//		printf("%p\n", &r);
+//	return 0;
+//}
+//测试大漠注册工具
+int main()
+{
+	return 0;
+}
